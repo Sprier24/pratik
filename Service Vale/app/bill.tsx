8fbfb5f19922986f7b7033f71eda0725dd61679b
pra27tikp <pratikpatelspriers@gmail.com>
2025-05-19 16:10:33 +0530

    19/05/2025
    In user remove category and pan related code. change name user to engineer)

--- Service Vale/app/bill.tsx
+++ Service Vale/app/bill.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, ScrollView, Image, Alert, Modal } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, ScrollView, Image, Alert, Modal } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useLocalSearchParams } from 'expo-router';
 import { Query } from 'appwrite';
