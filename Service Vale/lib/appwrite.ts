2ff7ecab8ce611b85da6c1cc0a50aebfdc8c844e
pra27tikp <pratikpatelspriers@gmail.com>
2025-08-18 16:07:33 +0530

    18/08/2025 (Change database 2)

--- Service Vale/lib/appwrite.ts
+++ Service Vale/lib/appwrite.ts
@@ -2,8 +2,8 @@ import { Client, Account, Databases, Storage, Teams } from 'react-native-appwrit
 
 const client = new Client()
     .setEndpoint('https://fra.cloud.appwrite.io/v1')
-    .setProject('681b300f0018fdc27bdd')
-    .setPlatform('com.test.servicevale');
+    .setProject('servicevale-id')
+    .setPlatform('com.spriers.servicevale');
 
 export const account = new Account(client);
 export const databases = new Databases(client);
